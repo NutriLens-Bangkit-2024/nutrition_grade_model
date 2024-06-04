16422919c30136575ababf79f57e35928322e8fc
Rafael Simson Riston <rafaelsimsonriston@gmail.com>
2024-06-05 04:39:54 +0800

    Modified the predict function by adding a normalization process before predicting

--- app/model/model.py
+++ app/model/model.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
+
 import numpy as np
 from pathlib import Path
 import os
+from sklearn import preprocessing
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
 
-model = tf.keras.models.load_model(os.path.join(BASE_DIR, 'nutrigrade_model(1).h5'),compile=False)
+model = tf.keras.models.load_model(os.path.join(BASE_DIR, 'nutrigrade_model.h5'),compile=False)
 
 # classes = [
 #     "A",
@@ -17,9 +19,11 @@ model = tf.keras.models.load_model(os.path.join(BASE_DIR, 'nutrigrade_model(1).h
 
 def predict_nutriscore(energi, protein, lemak, karbohidrat, serat, natrium):
     index_to_grade = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E'}
-    input_data = np.array([[energi, protein, lemak, karbohidrat, serat, natrium]])
-    predict = model.predict(input_data)
+    input_data = np.array([[energi, protein, lemak, karbohidrat, serat, natrium]], dtype=np.float32)
+    normalized_arr = preprocessing.normalize(input_data)
+    predict = model.predict(normalized_arr)
     max_index = np.argmax(predict)
     grade = index_to_grade[max_index]
 
     return grade
+
