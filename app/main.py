cd5442258bd7a66de4745ee7a3a05199b263b3be
Rafael Simson Riston <rafaelsimsonriston@gmail.com>
2024-06-08 23:27:31 +0800

    Add the input serving size and the output calories per 100 grams.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from app.model.model import predict_nutriscore
 
 app = FastAPI()
 class NutritionIn(BaseModel):
+    takaran_saji: float
     energi: float
     protein: float
     lemak: float
@@ -13,6 +14,7 @@ class NutritionIn(BaseModel):
 
 class PredictionOut(BaseModel):
     score: str
+    cal_per_100g: int
 
 @app.get("/")
 def home():
@@ -20,10 +22,25 @@ def home():
 
 @app.post("/predict", response_model=PredictionOut)
 def predict(payload: NutritionIn):
-    score = predict_nutriscore(payload.energi, 
-                               payload.protein, 
-                               payload.lemak,
-                               payload.karbohidrat,
-                               payload.serat,
-                               payload.natrium)
-    return {"score": score}
+    try:
+        # Convert the nutrients to per 100g basis
+        energi_per_100g = payload.energi * 100 / payload.takaran_saji
+        protein_per_100g = payload.protein * 100 / payload.takaran_saji
+        karbohidrat_per_100g = payload.karbohidrat * 100 / payload.takaran_saji
+        serat_per_100g = payload.serat * 100 / payload.takaran_saji
+        natrium_per_100g = payload.natrium * 100 / payload.takaran_saji
+        lemak_per_100g = payload.lemak * 100 / payload.takaran_saji
+
+        # Calculate Nutri-Score
+        score = predict_nutriscore(energi_per_100g,
+                                   protein_per_100g,
+                                   lemak_per_100g,
+                                   karbohidrat_per_100g,
+                                   serat_per_100g,
+                                   natrium_per_100g)
+
+        # Return the Nutri-Score and calories per 100g
+        return {"score": score, "cal_per_100g": round(energi_per_100g)}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
